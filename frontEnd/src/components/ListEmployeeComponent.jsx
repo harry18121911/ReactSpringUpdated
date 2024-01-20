ccd8243e119ef78d6d4f720b38dc4f0474daea73
harry18121911 <harrytheripper1812@hotmail.com.ar>
2024-01-20 10:50:38 -0300

    v1

--- frontEnd/src/components/ListEmployeeComponent.jsx
+++ frontEnd/src/components/ListEmployeeComponent.jsx
@@ -1,5 +1,5 @@
 import {useEffect, useState} from "react"
-import { listEmployees } from '../services/EmployeeService';
+import { deleteEmployee, listEmployees } from '../services/EmployeeService';
 import {useNavigate} from 'react-router-dom'
 
 const ListEmployeeComponent = () => {
@@ -7,12 +7,16 @@ const ListEmployeeComponent = () => {
     const [employees, setEmployees] = useState([])
     const navigate = useNavigate();
     useEffect(()=> {
+        getAllEmployees()
+    },[])
+
+    function getAllEmployees(){
         listEmployees().then((response) =>{
             setEmployees(response.data)
         }).catch(error => {
             console.error(error);
         })
-    },[])
+    }
 
     function addNewEmployee(){
         navigate('/add-employee')
@@ -22,11 +26,20 @@ const ListEmployeeComponent = () => {
         navigate(`/edit-employee/${id}`)
     }
 
+    function removeEmployee(id) {
+        console.log(id);
+        deleteEmployee(id).then(() => {
+            getAllEmployees()
+        }).catch(error => {
+        console.error(error);
+        })
+    }
+
     return (
 
         <div className="container">
             <h2 className="text-center">List of Employees</h2>
-            <button className="btn btn-primary mb-2" onClick={addNewEmployee} >Algo</button>
+            <button className="btn btn-primary mb-2" onClick={addNewEmployee} >Add Employee</button>
             <table className="table table-striped table-bordered">
                 <thead>
                     <tr>
@@ -47,7 +60,9 @@ const ListEmployeeComponent = () => {
                                 <td>{employee.email}</td>
                                 <td>
                                     <button className="btn btn-info" onClick={()=>updateEmployee(employee.id)}>Update</button>
+                                    <button className="btn btn-danger" onClick={()=>removeEmployee(employee.id)}>Delete</button>
                                 </td>
+                                
                                 
                             </tr>
                         ))
